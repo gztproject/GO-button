ce31d78dbba02c3153dae130e309c2f3811c1946
Gapi <gapi@gzt.si>
2024-06-16 13:40:58 +0200

    Compiled custom bootloader

---
+++ bootloader/.dep/Pipe_AVR8.o.d
@@ -0,0 +1,4 @@
+LUFA/LUFA/Drivers/USB/Core/AVR8/Pipe_AVR8.o: \
+ LUFA/LUFA/Drivers/USB/Core/AVR8/Pipe_AVR8.c \
+ LUFA/LUFA/Drivers/USB/Core/AVR8/../USBMode.h
+LUFA/LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
